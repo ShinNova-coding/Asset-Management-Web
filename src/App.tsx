151c5d88b18f0d97ba6f16553876ad211e0c06ac
chawsusuwin1 <chawsusuwin1@gmail.com>
2026-05-11 15:39:14 +0630

    project folder setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { Button } from "./components/ui/button"
+import { Card } from "./components/ui/card"
 
 const App = () => {
   return (
     <div className="p-2 font-bold text-2xl">
       <Button variant="outline" className="bg-yellow-500">Hello React</Button>
+      <Card className="bg-indigo-200">login</Card>
     </div>
   )
 }
