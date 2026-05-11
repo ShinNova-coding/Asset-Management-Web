73adaffddbda2e826758b6ba838e60cc6df69ac1
bhonekhant-dev <bhonekhantkyaw.dev@gmail.com>
2026-05-11 13:51:22 +0630

    project setup

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Button } from "./components/ui/button"
+
+const App = () => {
+  return (
+    <div className="p-2 font-bold text-2xl">
+      <Button variant="outline" className="bg-yellow-500">Hello React</Button>
+    </div>
+  )
+}
+
+export default App
